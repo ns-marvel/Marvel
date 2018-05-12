49c202373dbe6b54497c5fbf467d4e617e6abd14
Neil Smith <trolly999@gmail.com>
2018-05-12 23:26:43 +0100

    Add toString method for ArrayAdapter

--- app/src/main/java/com/springwoodcomputers/marvel/database/entity/CharacterSearch.java
+++ app/src/main/java/com/springwoodcomputers/marvel/database/entity/CharacterSearch.java
@@ -3,11 +3,13 @@ package com.springwoodcomputers.marvel.database.entity;
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 
 import lombok.Getter;
 
-@Entity(tableName = "search_history")
+@Entity(tableName = "search_history",
+        indices = {@Index(value = "SEARCH_STRING", unique = true)})
 public class CharacterSearch {
 
     @ColumnInfo(name = "ID")
@@ -15,7 +17,7 @@ public class CharacterSearch {
     @Getter
     private long id;
 
-    @ColumnInfo(name= "SEARCH_STRING")
+    @ColumnInfo(name = "SEARCH_STRING")
     private String searchString;
 
     public CharacterSearch(long id, String searchString) {
@@ -30,5 +32,10 @@ public class CharacterSearch {
 
     public String getSearchString() {
         return searchString.toLowerCase();
+    }
+
+    @Override
+    public String toString() {
+        return searchString;
     }
 }
