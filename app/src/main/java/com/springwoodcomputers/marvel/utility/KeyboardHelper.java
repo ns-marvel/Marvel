40e13d57bdb61a03c5e5f7ef8c662881b4967c40
Neil Smith <trolly999@gmail.com>
2018-05-12 23:29:00 +0100

    Helper class for showing and hiding the soft keyboard

---
+++ app/src/main/java/com/springwoodcomputers/marvel/utility/KeyboardHelper.java
@@ -0,0 +1,25 @@
+package com.springwoodcomputers.marvel.utility;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeyboardHelper {
+
+    private KeyboardHelper() {
+    }
+
+    public static void hideSoftKeyboard(View view) {
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public static void showSoftKeyboard(View view) {
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+}
