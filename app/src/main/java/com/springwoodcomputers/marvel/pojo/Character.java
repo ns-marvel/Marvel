db53988848e4328ec9d3a19960bc26ccbde0af5d
Neil Smith <trolly999@gmail.com>
2018-05-13 19:34:13 +0100

    Implementing formatting for received data

--- app/src/main/java/com/springwoodcomputers/marvel/pojo/Character.java
+++ app/src/main/java/com/springwoodcomputers/marvel/pojo/Character.java
@@ -4,15 +4,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@Getter
 public class Character {
 
+    @Getter
     private int id;
+    @Getter
     private String name;
+    @Getter
     private String description;
+    @Getter
     private Thumbnail thumbnail;
+
     private ComicList comicList;
     private StoryList storyList;
     private EventList eventList;
     private SeriesList seriesList;
+
+    public int getComicListCount() {
+        return comicList == null ? 0 : comicList.getAvailable();
+    }
+
+    public int getStoryListCount() {
+        return storyList == null ? 0 : storyList.getAvailable();
+    }
+
+    public int getEventListCount() {
+        return eventList == null ? 0 : eventList.getAvailable();
+    }
+
+    public int getSeriesListCount() {
+        return seriesList == null ? 0 : seriesList.getAvailable();
+    }
 }
